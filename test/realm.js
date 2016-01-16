47e0530eebdd1cac42e090124422b6f5927c645f
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-16 21:57:30 +0200

    realm tests refactored

--- test/realm.js
+++ test/realm.js
@@ -1,36 +1,30 @@
 'use strict'
-
 const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const plugiator = require('plugiator')
+
 chai.use(sinonChai)
 
 const Remi = require('../')
 const realm = require('../lib/realm')
 
 describe('realm', function() {
-  it('should path realm property', function(done) {
-    function plugin1(app, options, next) {
-      expect(app.realm.plugin).to.eq('plugin1')
-      expect(app.realm.pluginOptions.foo).to.eq(1)
-      return next()
-    }
-    plugin1.attributes = {
-      name: 'plugin1',
-      version: '0.0.0',
-    }
-
+  it('should path realm property', function() {
     let app = {}
-    let plugins = [{
-      register: plugin1,
-      options: {foo: 1},
-    },]
-    new Remi({
+
+    let remi = new Remi({
       extensions: [realm],
-    }).register(app, plugins, function(err) {
-      expect(err).to.not.exist
-      done()
     })
+    return remi.register(app, [
+      {
+        register: plugiator.create('plugin1', (app, options) => {
+          expect(app.realm.plugin).to.eq('plugin1')
+          expect(app.realm.pluginOptions.foo).to.eq(1)
+        }),
+        options: {foo: 1},
+      },
+    ])
   })
 })
